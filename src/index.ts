bd03c5bf2f09a86565e5e1e4bd29597841726405
Thant Ko Zaw <thantmkou10@gmail.com>
2023-09-01 20:48:45 +0630

    Add logger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,39 @@
 import express from "express";
 import { apiRouter } from "./router";
+import { errorHandler } from "./common/error-handler";
+import { pinoHttp } from "pino-http";
+import { logger } from "./common/logger";
 
 const app = express();
-const logger = require("pino-http");
 
-app.use(logger());
+// http logger
+app.use(
+  pinoHttp({
+    autoLogging: true,
+    logger,
+    customLogLevel: function (req, res, err) {
+      if (res.statusCode >= 400 && res.statusCode < 500) {
+        return "warn";
+      }
+
+      if (res.statusCode >= 500 || err) {
+        return "error";
+      }
+
+      return "info";
+    },
+  }),
+);
 app.use("/api", apiRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.use(errorHandler);
+
 const server = app.listen(3000, () => {
-  console.log("Server running on port 3000");
+  logger.info(`Server is running on port :3000`);
 });
 
 ["SIGTERM", "SIGINT"].forEach((signal) => {
